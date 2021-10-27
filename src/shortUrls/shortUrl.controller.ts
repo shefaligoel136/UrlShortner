3ff8bc1c7fa92c5b2b6324d52841616858079919
shefali goel <goel136shefali@gmail.com>
2021-10-27 12:26:36 +0530

    fix: url shortner database

--- src/shortUrls/shortUrl.controller.ts
+++ src/shortUrls/shortUrl.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Res } from '@nestjs/common';
+
+import { Response } from 'express';
 
 import { ShortUrlService } from './shortUrl.service';
 
@@ -18,7 +20,9 @@ export class ShortUrlController {
   }
 
   @Get(':code')
-  async getLongUrl(@Param('code') urlCode: string) {
-    return { url: await this.shortUrlService.getLongUrlFromCode(urlCode) };
+  async getLongUrl(@Param('code') urlCode: string, @Res() res: Response) {
+    const url = await this.shortUrlService.getLongUrlFromCode(urlCode);
+    res.redirect(url);
+    // return { url: url };
   }
 }
